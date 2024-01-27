75e26cd3e951a4c95c4d9bda48fe9ac1e4c44945
Wallas Almeida <wallasalmeida607@gmail.com>
2024-01-27 14:56:47 -0300

    feat [Wallas]: Correction details in category

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -52,7 +52,7 @@ class CategoryController {
         name: req.body.name,
         slug: Slug(req.body.name),
         keywords: req.body.keywords,
-        imgPath: req.file.path,
+        imgPath: `/category/imgCategory/${req.file.filename}`,
       })
         .then((data) => {
           //res.send('cadastrado com sucesso');
@@ -81,80 +81,41 @@ class CategoryController {
       });
   }
 
+  edit(req, res) {
+    if (req.body.name != "" && req.body.keywords != [""]) {
+      Category.findOne({_id: req.body.id}).then((data) => {
+          data.name = req.body.name,
+          data.slug = Slug(req.body.name),
+          data.keywords = req.body.keywords
+          if (req.file && req.file.filename) { 
+            data.imgPath = `/category/imgCategory/${req.file.filename}`
+          }
+
+          data.save().then(() => {
+            res.redirect("/categoria/");
+          })
+        })
+        .catch(function (erro) {
+          res.send("Erro ao cadastrar" + erro);
+          console.log(erro);
+        });
+    } else {
+      res.status(400).send("Não há dados válidos para cadastrar.");
+    }
+  }
+
   details(req, res) {
     Category.findOne({ _id: req.params.id })
       .then(function (category) {
         if (!category) {
           return res.status(404).json({ error: "Categoria não encontrada" });
         }
+        res.status(200).json(category);
 
-        Profile.find({ category_id: category._id })
-          .then(function (profiles) {
-            console.log({ profiles: profiles, category: category });
-            res.status(200).json({ profiles: profiles, category: category });
-          })
-          .catch(function (error) {
-            console.error("Erro ao buscar perfis:", error);
-            res.status(500).json({ error: "Erro interno do servidor" });
-          });
       })
       .catch(function (error) {
         console.error("Erro ao buscar categoria:", error);
         res.status(500).json({ error: "Erro interno do servidor" });
-      });
-  }
-
-  addProfile(req, res) {
-    Category.findOne({ _id: req.params.id }).then(function (category) {
-      let categoryID = category._id;
-
-      if (req.body && req.body.profile && Array.isArray(req.body.profile)) {
-        req.body.profile.forEach(function (oneUsername) {
-          const username = oneUsername;
-          try {
-            fetch(`http://127.0.0.1:5001/api/verifyUsername/${username}`)
-              .then((response) => response.json())
-              .then((data) => {
-                data = JSON.parse(data);
-                Profile.create({
-                  name: data.nome_usuario,
-                  userIdentify: username,
-                  urlImg: data.url_imagem_perfil,
-                  category_id: categoryID,
-                })
-                  .then(function () { })
-                  .catch(function (erro) {
-                    res.send("Erro ao cadastrar" + erro);
-                    console.log(erro);
-                  });
-              })
-              .catch((error) => {
-                console.error("Erro ao chamar a API:", error);
-              });
-          } catch (error) {
-            console.error("Erro durante a execução do script Python:", error);
-          }
-        });
-        res.redirect(`/categoria/details/${{ categoryID }}`);
-      } else {
-        console.error(
-          "O corpo da requisição ou a propriedade Profile não estão definidos ou não são um array."
-        );
-      }
-    });
-  }
-
-  deleteProfile(req, res) {
-    Profile.deleteOne({ _id: req.params.id })
-      .then((result) => {
-        if (result.deletedCount > 0) {
-          return { message: sucess };
-        } else {
-          res.send("Essa postagem não existe");
-        }
-      })
-      .catch(function (erro) {
-        res.send(erro);
       });
   }
 }
--- routes/category.js
+++ routes/category.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController');
 const multer = require('multer');
+const path = require('path');
 
 // Configuração do Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../hub-news/public/imgCategory');  // Especifique o caminho onde deseja armazenar as imagens
+    const categoryImagePath = path.join(path.resolve(process.cwd()), 'public', 'category', 'imgCategory',);
+    cb(null, categoryImagePath);
   },
   filename: function (req, file, cb) {
     const ext = file.originalname.split('.').pop();
@@ -19,9 +21,8 @@ const upload = multer({ storage: storage });
 router.get('/', new CategoryController().index);
 router.get('/buscar/:id', new CategoryController().search);
 router.post('/create', upload.single('imgPath'), new CategoryController().create);
+router.post('/edit', upload.single('imgPath'), new CategoryController().edit);
 router.get('/delete/:id', new CategoryController().delete);
 router.get('/details/:id', new CategoryController().details);
-router.post('/addProfile/:id', new CategoryController().addProfile);
-router.delete('/deleteProfile/:id', new CategoryController().deleteProfile);
 
 module.exports = router;
